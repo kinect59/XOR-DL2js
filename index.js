e5ea6c938c9acb5da0c3a0bf03fbb75018135956
kinect59 <hazem.wannous@univ-lille1.fr>
2020-09-01 13:26:53 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-/*
+
 async function run() {
   // Create a simple model.
   const model = tf.sequential();
@@ -16,7 +16,7 @@ async function run() {
       model.predict(tf.tensor2d([20], [1, 1])).dataSync();
 }
 run();
-*/
+
 /*
 async function run() {
   // Create a simple model.
@@ -38,34 +38,8 @@ async function run() {
 }
 run();
 */
+
 /*
-//var tf = require('@tensorflow/tfjs');
-
-async function train_test() {
-
-const model = tf.sequential();
-model.add(tf.layers.dense({units: 10, activation: 'sigmoid',inputShape: [2]}));
-model.add(tf.layers.dense({units: 1, activation: 'sigmoid',inputShape: [10]}));
-
-model.compile({loss: 'meanSquaredError', optimizer: 'rmsprop'});
-
-const training_data = tf.tensor2d([[0,0],[0,1],[1,0],[1,1]]);
-const target_data = tf.tensor2d([[0],[1],[1],[0]]);
-
-for (let i = 1; i < 100 ; ++i) {
- var h = await model.fit(training_data, target_data, {epochs: 30});
-   console.log("Loss after Epoch " + i + " : " + h.history.loss[0]);
-}
-
- model.predict(training_data).print();
-
-
-}
-
-train_test();
-*/
-
-
 // Tiny TFJS train / predict example.
 async function laod_and_test() {
  
@@ -79,3 +53,4 @@ async function laod_and_test() {
 }
 
 laod_and_test();
+*/
